dbfb67cd05990f9d6ebf3a75f9080f1cb75b03e2
Boris Chazalet <bchazalet@companywatch.net>
2021-04-11 14:35:26 +0200

    First commit with OneOrTwo

---
+++ src/main/scala/OneOrTwo.scala
@@ -0,0 +1,46 @@
+import scala.compiletime.{ error, codeOf }
+
+sealed abstract class OneOrTwo(val code: String)
+
+object OneOrTwo {
+
+    case object One extends OneOrTwo("one")
+    case object Two extends OneOrTwo("two")
+
+    def from(s: String): Option[OneOrTwo] = 
+        s match {
+            case One.code => Some(One)
+            case Two.code => Some(Two)
+            case _ => None
+        }
+
+    inline def inlinedFrom1(s: String): OneOrTwo = {
+        inline s match {
+            case "one" => One
+            case "two" => Two
+            case _ => error("can't make a OneOrTwo out of " + codeOf(s))
+        } 
+    }
+
+    val test1 = OneOrTwo.inlinedFrom1("one") // compiles 
+    val test12 = OneOrTwo.inlinedFrom1("three") // doesn't compile as expected -> can't make a OneOrTwo out of "three"
+
+     // this goes in the OneOrTwo companion object as well
+
+    inline def inlinedFrom2(s: String): OneOrTwo = {
+        from(s).getOrElse(error("can't make a OneOrTwo out of " + codeOf(s)))
+    }
+
+    inline def inlinedFrom3(s: String): OneOrTwo = {
+        s match {
+            case One.code => One
+            case Two.code => Two
+            case _ => error("can't make a OneOrTwo out of " + codeOf(s))
+        }
+    }
+
+    
+    val test2 = OneOrTwo.inlinedFrom2("one") // doesn't compile -> can't make a OneOrTwo out of "one"
+    val test3 = OneOrTwo.inlinedFrom3("one") // doesn't compile -> can't make a OneOrTwo out of "one"
+    
+}
